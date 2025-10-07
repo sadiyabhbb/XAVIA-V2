d1d8345b2b27f7df2916abb2b3056e604efc8947
sadiyabhbb <sadiyabhbb@proton.me>
2025-10-08 01:06:31 +0600

    Update noprefix.js

--- plugins/onMessage/noprefix.js
+++ plugins/onMessage/noprefix.js
@@ -1,6 +1,6 @@
 // coded by @aryannix. 
 
-const noPrefix = ["ai", "gemini"] //names that does not need a prefix
+const noPrefix = ["ai", "gemini", " bot"] //names that does not need a prefix
 
 
 async function onCall(methods) {
